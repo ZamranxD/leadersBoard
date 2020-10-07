035cb658cc96a98ea051a5afd6f6cc53fd7e81a3
ZamranxD <zamran.butt.1@gmail.com>
2020-10-07 14:10:02 +0500

    added an increase score functionality

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <h1 className="leadersboard">Leadersboard</h1>
-      <Student name="Mohammad Zamran" university="VU Pakistan" score="350" />
-      <Student name="Matt Murdock" university="LUMS" score="349" />
-      <Student name="John Doe" university="NUST" score="343" />
+      <Student name="Mohammad Zamran" university="VU Pakistan" score={350} />
+      <Student name="Matt Murdock" university="LUMS" score={343} />
+      <Student name="John Doe" university="NUST" score={338} />
     </div>
   );
 }
--- src/components/Student.js
+++ src/components/Student.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react'
 import "./student.css"
 
 export default class Student extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            score : this.props.score,
+        }
+    }
+
+    addScore(){
+        this.setState({
+            score : this.state.score +1,
+        })
+    }
+
     render() {
         return (
             <div className="student">
                 <div className="left">
                     <h2 className="name">
                         {this.props.name}
-                        <button className="addScore">+</button>
+                        <button className="addScore" onClick={() => this.addScore()} >+</button>
                     </h2>
                     
                     <p className="univerityName">{this.props.university}</p>
                 </div>
                 <div className="right">
                     <div className="score">
-                        {this.props.score}
+                        {this.state.score}
                     </div>
                 </div>
             </div>
